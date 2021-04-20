6f3bc2714835db16e97f867c704ff328b95df290
Songtech-0912 <jacky.song1020@gmail.com>
2021-04-20 15:45:40 -0400

    Added partial KaTeX support to chem notes

--- docs/pages/chemistry/script.js
+++ docs/pages/chemistry/script.js
@@ -1,20 +1,22 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   renderMathInElement(document.body, [
-//     { left: "$$", right: "$$", display: true },
-//     { left: "$", right: "$", display: false },
-//     { left: "\\(", right: "\\)", display: false },
-//     { left: "\\[", right: "\\]", display: true },
-//   ]);
-// });
-MathJax = {
-  tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']]
-  },
-  svg: {
-    fontCache: 'global'
-  }
-};
-
+// Setup Marked to support math equations with KaTeX
+// Generate HTML from markdown
 inputMD = document.getElementById("markdown").innerText;
 generatedHTML = marked(inputMD);
 document.getElementById("content").innerHTML = generatedHTML;
+
+// Render math equations using KaTeX
+document.addEventListener("DOMContentLoaded", function() {
+  renderMathInElement(document.body, {
+    // customised options
+    // • auto-render specific keys, e.g.:
+    delimiters: [
+      {left: '$$', right: '$$', display: true},
+      {left: '$', right: '$', display: false},
+      {left: '\\(', right: '\\)', display: false},
+      {left: '\\[', right: '\\]', display: true}
+    ],
+    // • rendering keys, e.g.:
+    throwOnError: false,
+    newLineInDisplayMode: true,
+  });
+});
