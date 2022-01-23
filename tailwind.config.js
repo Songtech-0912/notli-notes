0b2e0d82eaffde3320b6141a07e979b8e20e9ed5
Jacky Song <dixinlily@hotmail.com>
2022-01-23 08:30:11 -0500

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ["*.html", "./pages/**/*.html"],
+    content: ["*.html", "./pages/*.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
